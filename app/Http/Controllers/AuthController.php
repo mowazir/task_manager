837f1057809c88f5ac421985e6d95d27ec26baec
mowazir <abdussamadoduola@gmail.com>
2026-04-16 15:29:44 +0100

    make session component available

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -12,10 +13,33 @@ class AuthController extends Controller
         return view('auth.login');
     }
 
-    public function showRegisterationForm()
+    public function showRegisterForm()
     {
         return view('auth.register');
     }
+
+    public function login(Request $request)
+    {
+
+        $validatedResultData = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+            'remember' => ['nullable', 'boolean'],
+        ],
+        [
+            'email.required' => 'Please enter email',
+            'email.email' => 'Email entered not valid',
+            'password.required' => 'At least 3 digit'
+        ]
+        );
+
+        //Authentication logic..
+        if(Auth::attempt($request->only('email', 'password'), $request->boolean('remember') )){
+
+        }
+
+    }
+
 
 
 }
--- routes/web.php
+++ routes/web.php
@@ -8,18 +8,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//only accesible by users not logged in -guests-
+// guards determine how users are authenticated for each request (just like gatekeeper, determining the surety of the request)
+// providers defines how users are retrieved from persistent storage (how do i fetch user data from db)
+// middleware acts as filters that can inspect and modify request before they reach controllers ()
 
 Route::middleware('guest')->group(function() {
     Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
     Route::post('/login', [AuthController::class, 'login'])->name('login.post');
     Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
-    Route::post('/register', [AuthController::class, 'register'])->name('register.post');
+  //  Route::post('/register', [AuthController::class, 'register'])->name('register.post');
 });
 
 Route::middleware('auth')->group(function(){
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+   // Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+  //  Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
     Route::redirect('/', '/dashboard');
 });
